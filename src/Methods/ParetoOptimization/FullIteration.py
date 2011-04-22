2bb66d64db76191724be38dfe1304ffe16c05d2e
Daniel Zorin <Daniel Zorin@localhost>
2011-04-22 15:21:53 +0400

    [+] Documentation for Methods.ParetoOptimization

--- src/Methods/ParetoOptimization/FullIteration.py
+++ src/Methods/ParetoOptimization/FullIteration.py
@@ -3,8 +3,9 @@ from Core.Common import *
 from Systems.SystemGenerator import SystemGenerator
 import copy, pickle
 
-# Returns a list of all possible variants of the system.
 def GenerateSolutions(system):
+    ''' Returns a list of all possible variants of the system. '''
+    
     result = []
     v, max = system.Encode()
     for i in range(len(v)):
@@ -33,11 +34,9 @@ def GenerateSolutions(system):
         result = tmp
     return final 
 
-
-
-# Returns a list of reliability/cost values of Pareto front and 
-# a list of the corresponding solutions.
 def GenerateParetoFront(system):
+    ''' Returns a list of reliability/cost values of Pareto front and 
+    a list of the corresponding solutions.'''
     result = []
     v, max = system.Encode()
     for i in range(len(v)):
@@ -54,45 +53,45 @@ def GenerateParetoFront(system):
         for s in result:
             j += 1
             for k1 in range(1, max[i]["software"]+1):
-                 for k2 in range(1, max[i]["hardware"]+1):                 
-                     new = copy.deepcopy(s)
-                     new[i]["software"] = k1
-                     new[i]["hardware"] = k2
-                     if system.TestSolution(new) == False:
-                         pass
-                     else:
-                         system.Decode(new)
-                         rel = system.GetReliability()
-                         cost = system.GetCost()
-                         a = b = False
-                         tmppareto = []
-                         tmppareto2 = []
-                         index = 0
-                         for cur in paretofront:
-                             if Dominates(cur, {"rel":rel, "cost":cost}):
-                                 a = True
-                             if DominatesStrictly({"rel":rel, "cost":cost}, cur):
-                                 b = True
-                             if b == False:
-                                 tmppareto.append(cur)
-                                 tmppareto2.append(paretosolutions[index])
-                             index += 1
-                             b = False
-                         if a == False:
-                             tmppareto.append({"rel":rel, "cost":cost})
-                             tmppareto2.append(new)
-                         paretofront = tmppareto
-                         paretosolutions = tmppareto2
-                         tmp.append(new)
+                for k2 in range(1, max[i]["hardware"]+1):                 
+                    new = copy.deepcopy(s)
+                    new[i]["software"] = k1
+                    new[i]["hardware"] = k2
+                    if system.TestSolution(new) == False:
+                        pass
+                    else:
+                        system.Decode(new)
+                        rel = system.GetReliability()
+                        cost = system.GetCost()
+                        a = b = False
+                        tmppareto = []
+                        tmppareto2 = []
+                        index = 0
+                        for cur in paretofront:
+                            if Dominates(cur, {"rel":rel, "cost":cost}):
+                                a = True
+                            if DominatesStrictly({"rel":rel, "cost":cost}, cur):
+                                b = True
+                            if b == False:
+                                tmppareto.append(cur)
+                                tmppareto2.append(paretosolutions[index])
+                            index += 1
+                            b = False
+                        if a == False:
+                            tmppareto.append({"rel":rel, "cost":cost})
+                            tmppareto2.append(new)
+                        paretofront = tmppareto
+                        paretosolutions = tmppareto2
+                        tmp.append(new)
             if j % 100 == 0:
                 print(j, "/", l)
         result = tmp
         #print result
     return paretofront, paretosolutions
 
-# Returns a list of reliability/cost values of Pareto front and 
-# a list of the corresponding solutions.
 def GenerateParetoFrontOptimized(system):
+    ''' Returns a list of reliability/cost values of Pareto front and 
+    a list of the corresponding solutions.'''
     def next(v, max):
         new = copy.deepcopy(v)
         for i in range(len(v)):
@@ -118,37 +117,37 @@ def GenerateParetoFrontOptimized(system):
     paretofront = []
     paretosolutions = []
     while True: 
-         i += 1
-         if i % 1000 == 0:
+        i += 1
+        if i % 1000 == 0:
             print(i, "/", total)    
-         new = next(v, max)
-         v = new
-         if system.TestSolution(new) == False:
-             pass
-         else:
-             system.Decode(new)
-             rel = system.GetReliability()
-             cost = system.GetCost()
-             a = b = False
-             tmppareto = []
-             tmppareto2 = []
-             index = 0
-             for cur in paretofront:
-                 if Dominates(cur, {"rel":rel, "cost":cost}):
-                     a = True
-                 if Dominates({"rel":rel, "cost":cost}, cur):
-                     b = True
-                 if b == False:
-                     tmppareto.append(cur)
-                     tmppareto2.append(paretosolutions[index])
-                 index += 1
-                 b = False
-             if a == False:
-                 tmppareto.append({"rel":rel, "cost":cost})
-                 tmppareto2.append(new)
-             paretofront = tmppareto
-             paretosolutions = tmppareto2
-         if v == max:
-             break 
+        new = next(v, max)
+        v = new
+        if system.TestSolution(new) == False:
+            pass
+        else:
+            system.Decode(new)
+            rel = system.GetReliability()
+            cost = system.GetCost()
+            a = b = False
+            tmppareto = []
+            tmppareto2 = []
+            index = 0
+            for cur in paretofront:
+                if Dominates(cur, {"rel":rel, "cost":cost}):
+                    a = True
+                if Dominates({"rel":rel, "cost":cost}, cur):
+                    b = True
+                if b == False:
+                    tmppareto.append(cur)
+                    tmppareto2.append(paretosolutions[index])
+                index += 1
+                b = False
+            if a == False:
+                tmppareto.append({"rel":rel, "cost":cost})
+                tmppareto2.append(new)
+            paretofront = tmppareto
+            paretosolutions = tmppareto2
+        if v == max:
+            break 
     return paretofront, paretosolutions
     
--- src/Methods/ParetoOptimization/SPEA.py
+++ src/Methods/ParetoOptimization/SPEA.py
@@ -7,6 +7,7 @@ import copy, pickle
 import random
 
 class SPEA(GeneticAlgorithm):
+    '''.. warning:: Describe the algorithm here'''
     
     def __init__(self, system):
         GeneticAlgorithm.__init__(self, system)
