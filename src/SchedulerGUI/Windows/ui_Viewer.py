03e8a5cc65d17906bd27719abd203c47e20d6ce6
Daniel Zorin <Daniel Zorin@localhost>
2012-01-12 17:43:43 +0400

    [+] Main window GUI template

--- src/SchedulerGUI/Windows/ui_Viewer.py
+++ src/SchedulerGUI/Windows/ui_Viewer.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'Viewer.ui'
 #
-# Created: Tue Jan 10 19:15:02 2012
+# Created: Thu Jan 12 17:32:24 2012
 #      by: PyQt4 UI code generator 4.8.4
 #
 # WARNING! All changes made in this file will be lost!
