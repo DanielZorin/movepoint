5b3ef8bd69201b670a158599183e44f0fb15232e
Daniel Zorin <Daniel Zorin@localhost>
2012-02-06 14:31:01 +0400

    [~] exe build script updated

--- src/ExeBuild.py
+++ src/ExeBuild.py
@@ -10,4 +10,7 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-setup(name = "scheduler", version = "0.1", description = "scheduler", executables = [Executable("main.py", base=base)])
+if sys.argv[-1] != "build":
+    sys.argv.append("build")
+    
+setup(name = "scheduler", version = "0.1", description = "scheduler", executables = [Executable("SchedulerGUI-launch.py", base=base)])
