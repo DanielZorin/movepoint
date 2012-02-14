9fb24dd7dd4adfee6973af90729c7a1b34666acf
Daniel Zorin <Daniel Zorin@localhost>
2012-02-14 10:57:12 +0400

    tmp commit

--- src/SchedulerGUI/Windows/ui_PreferencesDialog.py
+++ src/SchedulerGUI/Windows/ui_PreferencesDialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'PreferencesDialog.ui'
 #
-# Created: Sun Feb 12 16:52:56 2012
+# Created: Sun Feb 12 18:28:20 2012
 #      by: PyQt4 UI code generator 4.8.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -17,19 +17,29 @@ except AttributeError:
 class Ui_PreferencesDialog(object):
     def setupUi(self, PreferencesDialog):
         PreferencesDialog.setObjectName(_fromUtf8("PreferencesDialog"))
-        PreferencesDialog.resize(182, 209)
+        PreferencesDialog.resize(182, 255)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(PreferencesDialog.sizePolicy().hasHeightForWidth())
         PreferencesDialog.setSizePolicy(sizePolicy)
+        PreferencesDialog.setMinimumSize(QtCore.QSize(182, 255))
+        PreferencesDialog.setMaximumSize(QtCore.QSize(182, 255))
         PreferencesDialog.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
         self.horizontalLayout_2 = QtGui.QHBoxLayout(PreferencesDialog)
+        self.horizontalLayout_2.setSizeConstraint(QtGui.QLayout.SetFixedSize)
         self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
         self.verticalLayout = QtGui.QVBoxLayout()
         self.verticalLayout.setSizeConstraint(QtGui.QLayout.SetDefaultConstraint)
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.groupBox = QtGui.QGroupBox(PreferencesDialog)
+        self.tabWidget = QtGui.QTabWidget(PreferencesDialog)
+        self.tabWidget.setObjectName(_fromUtf8("tabWidget"))
+        self.tab = QtGui.QWidget()
+        self.tab.setGeometry(QtCore.QRect(0, 0, 156, 175))
+        self.tab.setObjectName(_fromUtf8("tab"))
+        self.horizontalLayout_4 = QtGui.QHBoxLayout(self.tab)
+        self.horizontalLayout_4.setObjectName(_fromUtf8("horizontalLayout_4"))
+        self.groupBox = QtGui.QGroupBox(self.tab)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -50,6 +60,7 @@ class Ui_PreferencesDialog(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
         self.label.setSizePolicy(sizePolicy)
+        self.label.setMaximumSize(QtCore.QSize(16777215, 25))
         self.label.setObjectName(_fromUtf8("label"))
         self.verticalLayout_6.addWidget(self.label)
         self.label_2 = QtGui.QLabel(self.groupBox)
@@ -58,6 +69,7 @@ class Ui_PreferencesDialog(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
         self.label_2.setSizePolicy(sizePolicy)
+        self.label_2.setMaximumSize(QtCore.QSize(16777215, 25))
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.verticalLayout_6.addWidget(self.label_2)
         self.label_3 = QtGui.QLabel(self.groupBox)
@@ -66,6 +78,7 @@ class Ui_PreferencesDialog(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.label_3.sizePolicy().hasHeightForWidth())
         self.label_3.setSizePolicy(sizePolicy)
+        self.label_3.setMaximumSize(QtCore.QSize(16777215, 25))
         self.label_3.setObjectName(_fromUtf8("label_3"))
         self.verticalLayout_6.addWidget(self.label_3)
         self.label_4 = QtGui.QLabel(self.groupBox)
@@ -74,6 +87,7 @@ class Ui_PreferencesDialog(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.label_4.sizePolicy().hasHeightForWidth())
         self.label_4.setSizePolicy(sizePolicy)
+        self.label_4.setMaximumSize(QtCore.QSize(16777215, 25))
         self.label_4.setObjectName(_fromUtf8("label_4"))
         self.verticalLayout_6.addWidget(self.label_4)
         self.horizontalLayout_7.addLayout(self.verticalLayout_6)
@@ -97,7 +111,13 @@ class Ui_PreferencesDialog(object):
         self.verticalLayout_4.addWidget(self.lastop)
         self.horizontalLayout_7.addLayout(self.verticalLayout_4)
         self.horizontalLayout_3.addLayout(self.horizontalLayout_7)
-        self.verticalLayout.addWidget(self.groupBox)
+        self.horizontalLayout_4.addWidget(self.groupBox)
+        self.tabWidget.addTab(self.tab, _fromUtf8(""))
+        self.tab_2 = QtGui.QWidget()
+        self.tab_2.setGeometry(QtCore.QRect(0, 0, 156, 175))
+        self.tab_2.setObjectName(_fromUtf8("tab_2"))
+        self.tabWidget.addTab(self.tab_2, _fromUtf8(""))
+        self.verticalLayout.addWidget(self.tabWidget)
         self.horizontalLayout = QtGui.QHBoxLayout()
         self.horizontalLayout.setSizeConstraint(QtGui.QLayout.SetDefaultConstraint)
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
@@ -111,12 +131,13 @@ class Ui_PreferencesDialog(object):
         self.horizontalLayout_2.addLayout(self.verticalLayout)
 
         self.retranslateUi(PreferencesDialog)
+        self.tabWidget.setCurrentIndex(0)
         QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL(_fromUtf8("clicked()")), PreferencesDialog.OK)
-        QtCore.QObject.connect(self.pushButton_2, QtCore.SIGNAL(_fromUtf8("clicked()")), PreferencesDialog.Cancel)
-        QtCore.QObject.connect(self.axis, QtCore.SIGNAL(_fromUtf8("clicked()")), PreferencesDialog.AxisColor)
         QtCore.QObject.connect(self.deliveries, QtCore.SIGNAL(_fromUtf8("clicked()")), PreferencesDialog.DeliveriesColor)
-        QtCore.QObject.connect(self.tasks, QtCore.SIGNAL(_fromUtf8("clicked()")), PreferencesDialog.TaskColor)
+        QtCore.QObject.connect(self.axis, QtCore.SIGNAL(_fromUtf8("clicked()")), PreferencesDialog.AxisColor)
+        QtCore.QObject.connect(self.pushButton_2, QtCore.SIGNAL(_fromUtf8("clicked()")), PreferencesDialog.Cancel)
         QtCore.QObject.connect(self.lastop, QtCore.SIGNAL(_fromUtf8("clicked()")), PreferencesDialog.LastOpColor)
+        QtCore.QObject.connect(self.tasks, QtCore.SIGNAL(_fromUtf8("clicked()")), PreferencesDialog.TaskColor)
         QtCore.QMetaObject.connectSlotsByName(PreferencesDialog)
 
     def retranslateUi(self, PreferencesDialog):
@@ -125,7 +146,9 @@ class Ui_PreferencesDialog(object):
         self.label.setText(QtGui.QApplication.translate("PreferencesDialog", "Axis", None, QtGui.QApplication.UnicodeUTF8))
         self.label_2.setText(QtGui.QApplication.translate("PreferencesDialog", "Tasks", None, QtGui.QApplication.UnicodeUTF8))
         self.label_3.setText(QtGui.QApplication.translate("PreferencesDialog", "Deliveries", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_4.setText(QtGui.QApplication.translate("PreferencesDialog", "Last operation", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_4.setText(QtGui.QApplication.translate("PreferencesDialog", "Selection", None, QtGui.QApplication.UnicodeUTF8))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QtGui.QApplication.translate("PreferencesDialog", "Viewer", None, QtGui.QApplication.UnicodeUTF8))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QtGui.QApplication.translate("PreferencesDialog", "Graph Editor", None, QtGui.QApplication.UnicodeUTF8))
         self.pushButton.setText(QtGui.QApplication.translate("PreferencesDialog", "OK", None, QtGui.QApplication.UnicodeUTF8))
         self.pushButton_2.setText(QtGui.QApplication.translate("PreferencesDialog", "Cancel", None, QtGui.QApplication.UnicodeUTF8))
 
