d7a3e4c20c26a1fa8455f68cda97bbad84ad80fa
Daniel Zorin <Daniel Zorin@localhost>
2012-07-11 20:24:06 +0400

    [~] SRGM GUI fixed

--- src/SRGMGUI/ui.py
+++ src/SRGMGUI/ui.py
@@ -3,4 +3,5 @@ from PyQt4 import uic
 if __name__ == "__main__":
     fin = open("main.ui", "r")
     fout = open("ui_main.py", "w")
-    uic.compileUi(fin, fout)
+    uic.compileUi(fin, fout, from_imports=True)
+    
