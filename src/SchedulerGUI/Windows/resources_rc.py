1e9886ab682174091d6c9213caef470015074a2b
Daniel Zorin <Daniel Zorin@localhost>
2012-08-06 21:56:14 +0400

    [~] plugins update

--- src/SchedulerGUI/Windows/resources_rc.py
+++ src/SchedulerGUI/Windows/resources_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: ?? 5. ??? 22:10:49 2012
+# Created: ?? 6. ??? 21:20:06 2012
 #      by: The Resource Compiler for PyQt (Qt v4.8.2)
 #
 # WARNING! All changes made in this file will be lost!
