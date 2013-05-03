3d4da95b33ac7730b4ec68612eded08f684060ae
Daniel Zorin <Daniel Zorin@localhost>
2013-05-03 23:06:59 +0600

    Realistic antenna generator

--- src/plugins/Antenna.py
+++ src/plugins/Antenna.py
@@ -3,6 +3,7 @@ from Schedules.ProgramVertex import ProgramVertex
 from Schedules.ProgramEdge import ProgramEdge
 from Core.Version import *
 from Core.Processor import *
+import math
 
 class AntennaGenerator:
     a = 2.5
@@ -10,10 +11,8 @@ class AntennaGenerator:
     K = 10
     L = 2
     n = 5
-    steps = 3
+    steps = 1
     Mtheta = 2
-    FFT = 3
-    BCM = 10
     perf = 100
     bandwidth = 1
     rel = 0.9
@@ -33,7 +32,6 @@ class AntennaGenerator:
 
         # Calculate tdir
         s.tdir = self.a * self.B * self.n
-        self.BCM = int(s.tdir / (self.steps * (3 + self.Mtheta) + 5))
 
         # Create processors
 
@@ -48,52 +46,66 @@ class AntennaGenerator:
             self.vCount += 1
             s.program.vertices.append(newv)
             return newv
-
+        x = self.L
+        y = self.K
+        if x < y:
+            x, y = y, x
+        while y != 0:
+            x, y = y, x % y
+        gcd = x
         fft = []
         for i in range(self.K):
-            v = newVertex(self.FFT)
-            v.name = "FFT_" + str(i + 1)
-            fft.append(v)
+            v = newVertex(int(self.a * self.L / gcd))
+            v.name = "Norm_" + str(i + 1)
+            v2 = newVertex(int(self.a * self.L / gcd * math.log(self.a * self.L, 2)))
+            v2.name = "FFT_" + str(i + 1)
+            v3 = newVertex(int(self.K * self.K / gcd))
+            v3.name = "Mult_" + str(i + 1)
+            e = ProgramEdge(v, v2, int(self.a * self.L))
+            s.program.edges.append(e)
+            e = ProgramEdge(v2, v3, 1)
+            s.program.edges.append(e)
+            fft.append(v3)
 
         # Create BCM
         last = []
         for i in range(self.L):
-            v = newVertex(self.BCM)
-            v.name = "CME_" + str(i + 1) + "_stage_1"
+            v = newVertex(int(self.K ** 3 / gcd))
+            v.name = "Eigenvalues_" + str(i + 1)
             for v0 in fft:
-                e = ProgramEdge(v0, v, 1)
+                e = ProgramEdge(v0, v, self.K ** 2)
                 s.program.edges.append(e)
-            for j in range(self.steps - 1):
-                v1 = newVertex(self.BCM)
+            for j in range(self.steps):
+                v1 = newVertex(int(self.K / gcd))
                 v1.name = "CME_" + str(i + 1) + "_stage_" + str(j + 2)
-                e = ProgramEdge(v, v1, 1)
+                e = ProgramEdge(v, v1, self.K ** 2)
                 s.program.edges.append(e)
                 v = v1
             prev = v
             lastpar = []
             for k in range(self.Mtheta):
-                v1 = newVertex(self.BCM)
-                v1.name = "CME_" + str(i + 1) + "_pstage_" + str(k) + "_1"
-                e = ProgramEdge(prev, v1, 1)
+                v1 = newVertex(int(self.K / gcd))
+                v1.name = "CME_" + str(i + 1) + "_pstage_" + str(k)
+                e = ProgramEdge(prev, v1, self.K)
                 s.program.edges.append(e)
                 v = v1
-                for j in range(self.steps - 2):
-                    v1 = newVertex(self.BCM)
+                for j in range(self.steps - 1):
+                    v1 = newVertex(int(self.K / gcd))
                     v1.name = "CME_" + str(i + 1) + "_pstage_" + str(k) + "_" + str(j + 2)
-                    e = ProgramEdge(v, v1, 1)
+                    e = ProgramEdge(v, v1, self.K)
                     s.program.edges.append(e)
                     v = v1
                 lastpar.append(v)
-            v = newVertex(self.BCM)
-            v.name = "CME_" + str(i + 1) + "_pstage_" + str(self.steps)
+            v = newVertex(int(self.K / gcd))
+            v.name = "Comparison_" + str(i + 1)
             for v0 in lastpar:
                 e = ProgramEdge(v0, v, 1)
                 s.program.edges.append(e)
             last.append(v)
-        v = newVertex(self.BCM)
+        v = newVertex(int(self.K * self.L / gcd))
         v.name = "CME_final"
         for v0 in last:
-            e = ProgramEdge(v0, v, 1)
+            e = ProgramEdge(v0, v, self.K)
             s.program.edges.append(e)
         last.append(v)           
         s.program._buildData()
