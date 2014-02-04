a72a743e7e330ae4c2be20b8ca785113ebc6ae1a
juan <juan@juan-??>
2014-02-04 16:20:01 +0300

    parser

--- src/AntennaTest.py
+++ src/AntennaTest.py
@@ -14,31 +14,33 @@ p = Project("program.xml", "temperature test")
 gen = AntennaGenerator()
 gen.K = 8
 res = ""
-for l in range(1, 100):
+for l in range(1, 70):
     for m in range(1, 4): 
-        gen.L = l + 1
+        gen.L = l * 2 + 1
         gen.Mtheta = m + 1 
         gen.B = 400
         gen.Generate(p.system)
         print (l + 1, m + 1)
-        try:
-            best = run(p, l, m)
-            ratio = str(float(best["processors"]) / float(l + 1) / float(m + 1)) + ";"
-        except:
-            ratio = "ERROR;"
-        f = open("results_antenna.txt", "a")
-        f.write("b200;" + str(l) + ";" + str(m) + ";" + str(best["processors"]) + ";" + ratio + str(best["time"]) + "\n")
-        f.close()
+        for i in range(20):
+            try:
+                best = run(p, l, m)
+                ratio = str(float(best["processors"]) / float(l + 1) / float(m + 1)) + ";"
+            except:
+                ratio = "ERROR;"
+            f = open("results_antenna.txt", "a")
+            f.write("b200;" + str(l * 2 + 1) + ";" + str(m) + ";" + str(i) + ";" + str(best["processors"]) + ";" + ratio + str(best["time"]) + "\n")
+            f.close()
 
         gen.B = 900
         gen.Generate(p.system)
         print (l + 1, m + 1)
-        try:
-            best = run(p, l, m)
-            ratio = str(float(best["processors"]) / float(l + 1) / float(m + 1)) + ";"
-        except:
-            ratio = "ERROR;"
-        f = open("results_antenna.txt", "a")
-        f.write("b900;" + str(l) + ";" + str(m) + ";" + str(best["processors"]) + ";" + ratio + str(best["time"]) + "\n")
-        f.close()
+        for i in range(20):
+            try:
+                best = run(p, l, m)
+                ratio = str(float(best["processors"]) / float(l + 1) / float(m + 1)) + ";"
+            except:
+                ratio = "ERROR;"
+            f = open("results_antenna.txt", "a")
+            f.write("b900;" + str(l * 2 + 1) + ";" + str(m) + ";" + str(i) + ";" + str(best["processors"]) + ";" + ratio + str(best["time"]) + "\n")
+            f.close()
     res += "\n"
